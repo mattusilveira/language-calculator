6ff3c932f5acfa02fcee546ba693176cf8c71d09
mattusilveira <matheus12062007silveira7@gmail.com>
2023-11-23 11:54:01 -0300

    first commit

--- math.js
+++ math.js
@@ -1,41 +1,41 @@
-let valor1 = document.querySelector("#Valor1");
-let valor2 = document.querySelector("#Valor2");
+let valor1 = document.querySelector("#valor1");
+let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-
-
-function soma(a, b) {
-    resultado.textContent(a + b);
-}
-
-function subtracao(a, b) {
-    resultado.textContent(a - b);
-}
-
-function multiplicacao(a, b) {
-    resultado.textContent(a * b);
-
-    function divisao(a, b) {
-        resultado.textContent(a / b);
-    }
-}
-
 let btSoma = document.querySelector("#btSoma");
-btSoma.addEventListener("click", function () {
-    soma(Number(valor1.value), Number(valor2.value));
-});
+btSoma.addEventListener("click",
+    function () {
+        soma(Number(valor1.value), Number(valor2.value));
+    });
 
 let btSubtracao = document.querySelector("#btSubtracao");
 btSubtracao.addEventListener("click", function () {
-    soma(Number(valor1.value), Number(valor2.value));
+    subtracao(Number(valor1.value), Number(valor2.value));
 })
 
 let btMultiplicacao = document.querySelector("#btMultiplicacao");
-btMultiplicacao.addEventListener("click", function(){
-    soma(Number(valor1.value), Number(valor2.value))
+btMultiplicacao.addEventListener("click", function () {
+    multiplicacao(Number(valor1.value), Number(valor2.value))
 })
 
 let btDivisao = document.querySelector("#btDivisao");
-btDivisao.addEventListener("click", function(){
-    soma(Number(valor1.value), Number(valor2.value))
-})
+btDivisao.addEventListener("click",
+    function () {
+        divicao(Number(valor1.value), Number(valor2.value))
+    })
+
+function soma(a, b) {
+    resultado.textContent = (a + b);
+}
+
+function subtracao(a, b) {
+    resultado.textContent = (a - b);
+}
+
+function multiplicacao(a, b) {
+    resultado.textContent = (a * b);
+}
+    function divicao(a, b) {
+        resultado.textContent = (a / b);
+  
+    }
